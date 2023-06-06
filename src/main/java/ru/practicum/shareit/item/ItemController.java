e63448cffafeeda277c7ee61f44fbfed5448b44f
mutaev <i.mutaev@gmail.com>
2023-06-07 02:09:11 +0300

    item add-controllers

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,37 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
 
 /**
  * TODO Sprint add-controllers.
  */
+@RequiredArgsConstructor
+@Slf4j
 @RestController
 @RequestMapping("/items")
 public class ItemController {
+    private final ItemService itemService;
+
+    @PostMapping
+    public ItemDto createItem(ItemDto item) {
+        return item;
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(ItemDto item) {
+        return item;
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItem(String id) {
+        return new ItemDto();
+    }
+
+    @GetMapping("/search?text={text}")
+    public ItemDto search(String text) {
+        return new ItemDto();
+    }
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,10 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * TODO Sprint add-controllers.
  */
+@RequiredArgsConstructor
 public class ItemDto {
 }
