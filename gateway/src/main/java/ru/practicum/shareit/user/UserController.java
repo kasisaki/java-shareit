9a65599cb8f1a642142fe990b28259c254fdfe6d
mutaev <i.mutaev@gmail.com>
2023-06-30 07:14:17 +0300

    load

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserUpdateDto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserClient userClient;
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@Valid @RequestBody UserUpdateDto userUpdateDto) {
+        return userClient.createUser(userUpdateDto);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable long id,
+                                             @RequestBody UserUpdateDto userUpdateDto) {
+        return userClient.updateUser(userUpdateDto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable long id) {
+        return userClient.deleteUser(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findAllUsers(
+            @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") int from,
+            @Positive @RequestParam(name = "size", defaultValue = "10") int size) {
+        return userClient.findAllUsers(from, size);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUser(@PathVariable(required = false) long id) {
+        return userClient.getUser(id);
+    }
+}
